d9d7004853e54d7fa90167a11722407e1517a3df
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-24 22:45:22 +0900

    initial commit

---
+++ src/test/java/work/contre/NoDI/NoDiApplicationTests.java
@@ -0,0 +1,13 @@
+package work.contre.NoDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
