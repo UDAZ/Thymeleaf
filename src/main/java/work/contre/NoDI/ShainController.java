e87e2b540a2119ed04810c77aef8b74ae25c183a
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-26 17:58:47 +0900

    add 24. 入力値チェックの実行

--- src/main/java/work/contre/NoDI/ShainController.java
+++ src/main/java/work/contre/NoDI/ShainController.java
@@ -2,22 +2,35 @@ package work.contre.NoDI;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ShainController {
 
+	@ModelAttribute
+	public ShainForm setUpForm() {
+		return new ShainForm();
+	}
+
 	@RequestMapping("/input")
-		public String index() {
+	public String index() {
 		return "index.html";
 	}
-	
+
 	@RequestMapping("/output")
-	public String result(@RequestParam("number") String number, Model model) {
+	public String result(@Validated ShainForm shainForm, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "index.html";
+		}
+
+		String number = shainForm.getNumber();
 		String name = "コントローラー太郎";
 		model.addAttribute("number", number);
 		model.addAttribute("name", name);
 		return "output.html";
 	}
+
 }
