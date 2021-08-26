9015cc857d8a3b1234c1284185eaaadb9531d968
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-08-26 18:07:49 +0900

    add 25. 入力値チェックの変更

--- src/main/java/work/contre/NoDI/ShainForm.java
+++ src/main/java/work/contre/NoDI/ShainForm.java
@@ -1,10 +1,12 @@
 package work.contre.NoDI;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class ShainForm {
 
 @NotEmpty(message = " 社員Noを入れてくださいね")
+@Pattern(regexp = "\\d{3}", message = "半角数字、3桁でお願いします")
 private String number;
 
 public String getNumber() {
